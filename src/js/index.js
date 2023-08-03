a6ed14f7f3d347676d78b64236b5be776c7c9e44
Jessica Oliveira <elisa0js@gmail.com>
2023-08-03 12:18:21 -0300

    conclusao js

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,33 @@
-const container= document.querySelector(".container")
-const labelError=document.querySelector(".label-error")
-const inputEmail=document.querySelector(".email")
-const btnSubscribe=document.querySelector(".btn-subscribe")
-const thanks=document.querySelector(".thanks")
+const container = document.querySelector(".container");
+const inputEmail = document.getElementById("email");
 
-btnSubscribe.addEventListener("click", (e)=>{
-    e.preventDefault()
-    validEmail()
-    
-   
-    })
+const btnSubscribe = document.querySelector(".btn-subscribe");
+const thanks = document.querySelector(".thanks");
+const labelError = document.querySelector(".label-error");
 
-  
-function validEmail(){
-    if(inputEmail.value){
-        container.style.display="none"
-        thanks.style.display="block"
-    }else{
-        inputEmail.classList.add("error")
-        labelError.style.display="block"
+btnSubscribe.addEventListener("click", (e) => {
+  e.preventDefault();
+  validEmail();
+});
+
+function validEmail() {
+  const email = inputEmail.value.trim();
+
+  const emailPattern = /^[^\s@]+@[^\s@]+.[^\s@]+$/;
+
+  if (!emailPattern.test(email)) {
+    labelError.style.display = "block";
+    inputEmail.classList.add("error");
+  } else {
+    labelError.style.display = "block";
+    inputEmail.classList.remove("error");
+
+    if (inputEmail.value) {
+      container.style.display = "none";
+      thanks.style.display = "block";
+    } else {
+      inputEmail.classList.add("error");
+      labelError.style.display = "block";
     }
+  }
 }
